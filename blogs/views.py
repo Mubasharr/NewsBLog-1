82360d6d7c0f153c7615fa064a51cbf0f44d59d9
Mubashar Hussain <46559255+Mubasharr@users.noreply.github.com>
2020-02-03 20:16:50 +0500

    Initial commit add django files

---
+++ blogs/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import NewsBlog
+
+
+def index(request):
+    news = NewsBlog.objects.all()
+    return render(request, 'index.html',
+                  {'news': news})
+# Nadeem AHmed
